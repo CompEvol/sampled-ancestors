9dc8ba0a8a9338c22df0ce74c4d6acbb62e23706
Alexandra Gavryushkina <gavryushkina@gmail.com>
2014-01-29 01:15:27 +0000

    FBD stand along

--- src/beast/evolution/speciation/FBDModel.java
+++ src/beast/evolution/speciation/FBDModel.java
@@ -11,9 +11,6 @@ import beast.evolution.tree.ZeroBranchSATree;
  */
 public class FBDModel extends SpeciesTreeDistribution {
 
-    public Input<RealParameter> originInput =
-            new Input<RealParameter>("origin", "The origin of infection", Input.Validate.REQUIRED);
-
     //'direct' parameters
     public Input<RealParameter> birthRateInput =
             new Input<RealParameter>("birthRate", "Birth rate");
@@ -31,22 +28,15 @@ public class FBDModel extends SpeciesTreeDistribution {
             new Input<RealParameter>("samplingProportion", "The probability of sampling prior to death. Sampling rate/(sampling rate + death rate)", Input.Validate.XOR, samplingRateInput);
 
 
-    // r parameter
-    public Input<RealParameter> becomeNoninfectiousAfterSamplingProbability =
-            new Input<RealParameter>("becomeNoninfectiousAfterSamplingProbability", "The probability of an individual to become noninfectious immediately after the sampling", Input.Validate.REQUIRED);
-
-    public Input<RealParameter> rhoProbability =
+    public Input<RealParameter> rhoInput =
             new Input<RealParameter>("rho", "Probability of an individual to be sampled at present", (RealParameter)null);
 
-
-    protected double r;
     protected double lambda;
     protected double mu;
     protected double psi;
+    protected double rho;
     protected double c1;
     protected double c2;
-    protected double origin;
-    protected double rho;
     protected boolean transform; //is true if the model is parametrised through transformed parameters
 
     public void initAndValidate() throws Exception {
@@ -54,9 +44,6 @@ public class FBDModel extends SpeciesTreeDistribution {
         if (birthRateInput.get() != null && deathRateInput.get() != null && samplingRateInput.get() != null) {
 
             transform = false;
-            //mu = deathRateInput.get().getValue();
-            //psi = samplingRateInput.get().getValue();
-            //lambda = birthRateInput.get().getValue();
 
         } else if (diversificationRateInput.get() != null && turnoverInput.get() != null && samplingProportionInput.get() != null) {
 
@@ -68,26 +55,13 @@ public class FBDModel extends SpeciesTreeDistribution {
 
     }
 
-    private double p0s(double t) {
-        double p0 = (lambda + mu + psi - c1 * ((1 + c2) - Math.exp(-c1 * t) * (1 - c2)) / ((1 + c2) + Math.exp(-c1 * t) * (1 - c2))) / (2 * lambda);
-        return r + (1 - r) * p0;
-    }
-
-    private double oneMinusP0Hat(double t) {
-        return rho*(lambda-mu)/(lambda*rho + (lambda*(1-rho) - mu)* Math.exp((mu-lambda) * t)) ;
-    }
-
-    private double q(double t) {
-        return Math.exp(c1 * t) * (1 + c2) * (1 + c2) + Math.exp(-c1 * t) * (1 - c2) * (1 - c2) + 2 * (1 - c2 * c2);
-    }
-
     private void transformParameters() {
         double d = diversificationRateInput.get().getValue();
         double r_turnover = turnoverInput.get().getValue();
         double s = samplingProportionInput.get().getValue();
         lambda = d/(1-r_turnover);
-        mu = r_turnover*lambda;
-        psi = mu*s/(1-s);
+        mu = r_turnover * d/(1-r_turnover);
+        psi =  (s/(1-s)) *  r_turnover * d/(1-r_turnover);
     }
 
     private void updateParameters() {
@@ -100,58 +74,55 @@ public class FBDModel extends SpeciesTreeDistribution {
             psi = samplingRateInput.get().getValue();
         }
 
-        r = becomeNoninfectiousAfterSamplingProbability.get().getValue();
-        if (rhoProbability.get() != null ) {
-            rho = rhoProbability.get().getValue();
-        } else {
-            rho = 0.;
-        }
-        c1 = Math.sqrt((lambda - mu - psi) * (lambda - mu - psi) + 4 * lambda * psi);
-        c2 = -(lambda - mu - 2*lambda*rho - psi) / c1;
-        origin = originInput.get().getValue();
+        rho = rhoInput.get().getValue();
+        c1 =  Math.abs(Math.sqrt((lambda-mu-psi)*(lambda-mu-psi)+4*lambda*psi));
+        c2 = -(lambda- mu - 2 * lambda*rho - psi)/c1;
+    }
+
+    private double q(double t){
+        return (1+c2)*(1+c2)*Math.exp(c1*t) + 2*(1-c2*c2) + (1-c2)*(1-c2)*Math.exp(-c1*t);
+    }
+
+    private double p0(double t){
+        return 1 + (-(lambda-mu-psi)- c1*(1+c2 - (1-c2)*Math.exp(-c1*t))/(1+c2 + (1-c2)*Math.exp(-c1*t)))/(2*lambda);
+    }
+
+    private double oneMinusP0Hat(double t){
+        return rho*(lambda - mu)/(lambda*rho + (lambda*(1-rho) - mu)*Math.exp(-(lambda-mu)*t));
     }
 
     @Override
-    public double calculateTreeLogLikelihood(TreeInterface tree)
-    {
-        int nodeCount = tree.getNodeCount();
-        updateParameters();
-        //double x0 = tree.getRoot().getHeight() + origToRootDistance;
-        if (origin < tree.getRoot().getHeight()) {
+    public double calculateTreeLogLikelihood(TreeInterface tree){
+
+        if (tree.getRoot().getHeight() > 10000) {
             return Double.NEGATIVE_INFINITY;
         }
 
-        double logPost = -Math.log(q(origin)) - Math.log(oneMinusP0Hat(origin));
+        double logPost=0.0;
 
+        updateParameters();
 
-        int internalNodeCount = tree.getLeafNodeCount() - ((ZeroBranchSATree)tree).getDirectAncestorNodeCount() - 1;
+        logPost -= 2*Math.log(lambda*oneMinusP0Hat(tree.getRoot().getHeight()));
+        logPost += Math.log(lambda) - Math.log(q(tree.getRoot().getHeight()));
 
-        logPost += Math.log(Math.pow(2, internalNodeCount));
-
-        for (int i = 0; i < nodeCount; i++) {
-            if (tree.getNode(i).isLeaf()) {
-                if  (!((ZeroBranchSANode)tree.getNode(i)).isDirectAncestor())  {
-                    if (tree.getNode(i).getHeight() > 0.000000000005 || rho == 0.) {
-                        logPost += Math.log(psi) + Math.log(q(tree.getNode(i).getHeight())) + Math.log(p0s(tree.getNode(i).getHeight()));
-                    } else {
-                        logPost += Math.log(4*rho);
-                    }
-                }
-            } else {
-                if (((ZeroBranchSANode)tree.getNode(i)).isFake()) {
-                    if (r == 1) {
-                        System.out.println("r = 1 but there are sampled ancestors in the tree");
-                        System.exit(0);
-                    }
-                    logPost += Math.log(psi) + Math.log(1 - r);
-                } else {
-                    logPost += Math.log(lambda) - Math.log(q(tree.getNode(i).getHeight()));
-                }
+        for (int i=0; i< tree.getNodeCount(); i++) {
+            ZeroBranchSANode node = (ZeroBranchSANode)tree.getNode(i);
+            if (node.isLeaf()){
+                 if (node.getHeight() > 0.000000000001) {
+                     if (node.isDirectAncestor()){
+                         logPost += Math.log(psi);
+                     }  else {
+                         logPost += Math.log(psi*2*p0(node.getHeight())*q(node.getHeight()));
+                     }
+                 }  else {
+                      logPost += Math.log(4*rho);
+                 }
+            }  else {
+                 if (!node.isFake()) {
+                     logPost += Math.log(lambda) - Math.log(q(node.getHeight()));
+                 }
             }
         }
-
-        //System.out.println("logpost = " + logPost + " 2 to the power = " + Math.pow(2, internalNodeCount));
-
         return logPost;
     }
 
