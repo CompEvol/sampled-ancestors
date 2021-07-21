59e958352dc00d2dedc93bf7a67fb904cc5b8b25
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2021-07-21 22:57:48 +0200

    Fix to corner cases

--- src/beast/evolution/tree/RandomTreeWithSA.java
+++ src/beast/evolution/tree/RandomTreeWithSA.java
@@ -1,20 +1,28 @@
 package beast.evolution.tree;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 import beast.core.Input;
+import beast.core.Input.Validate;
 import beast.core.util.Log;
 import beast.evolution.alignment.TaxonSet;
 
 public class RandomTreeWithSA extends RandomTree {
 
 	final public Input<TaxonSet> startAsSampledAncestors = new Input<>("sampledAncestor",
-			"Taxa to start as sampled ancestors");
+			"Taxa to start as sampled ancestors", Validate.REQUIRED);
 
 	@Override
 	public void initAndValidate() {
-		if (!taxaInput.get().getTaxaNames().containsAll(startAsSampledAncestors.get().getTaxaNames())) {
+		taxa = new LinkedHashSet<>();
+		if (taxaInput.get() != null) {
+			taxa.addAll(taxaInput.get().getTaxaNames());
+		} else {
+			taxa.addAll(m_taxonset.get().asStringList());
+		}
+		if (!taxa.containsAll(startAsSampledAncestors.get().getTaxaNames())) {
 			Set<String> remaining = startAsSampledAncestors.get().getTaxaNames();
 			remaining.removeAll(taxaInput.get().getTaxaNames());
 			Log.warning("WARNING: Not all sampledAncestor taxa were part of the alignment taxa. Missing taxa "
@@ -31,7 +39,7 @@ public class RandomTreeWithSA extends RandomTree {
 
 		for (Node leaf : root.getAllLeafNodes()) {
 
-			if (!sa.contains(this.getTaxonId(leaf))) {
+			if (!sa.contains(getTaxonId(leaf))) {
 				continue;
 			}
 			Node parent = leaf.getParent();
@@ -43,7 +51,11 @@ public class RandomTreeWithSA extends RandomTree {
 			// assumptions.
 			for (Node node : parent.getAllChildNodesAndSelf()) {
 				if (node.getHeight() > node.parent.getHeight()) {
-					if (sa.contains(getTaxonId(node))) {
+					if (!node.isLeaf()) {
+						// Not a leaf, so cannot be a sampled ancestor which needs to be handled with
+						// care
+						node.setHeight(node.parent.getHeight() - 1e-9);
+					} else if (sa.contains(getTaxonId(node))) {
 						// Keep sampled ancestors as sampled ancestors. Yes, this is a bit redundant,
 						// but at least it should be correct. The alternative is to recursively walk the
 						// tree in a somewhat erratic order.
@@ -55,6 +67,10 @@ public class RandomTreeWithSA extends RandomTree {
 				}
 			}
 			assert leaf.isDirectAncestor();
+		}
+
+		if (m_initial.get() != null) {
+			m_initial.get().assignFromWithoutID(this);
 		}
 	}
 }
